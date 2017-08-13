8eaf3decd66a7a59be33754f61045ef5f2467416
theeldestelder <joshuag98@gmail.com>
2017-08-13 17:23:43 -0500

    Initial commit
    basic code

---
+++ plotThePoints.cpp
@@ -0,0 +1,29 @@
+#include iostream
+#include fstream
+
+int main() // reads in a list of coordinates from a .csv file and uses Google Maps to draw those points on a map
+{
+	ifstream input;
+
+	input.open("Combined Data Set.csv"); 
+
+	string text = "";
+
+	open >> text >> text >> text >> text; // first line 
+
+
+	double latitude = 0;
+	double longitude = 0;
+	string description = "";
+	string place = "";
+
+
+	while(open >> longitude >> latitude >> place >> description) // assigns variables based on columns in csv file
+	{
+		//plot the points with the given data
+		cout << "longitude: " << longitude << endl;
+		cout << "latitude: " << latitude << endl;
+		cout << "place: " << place << endl;
+		cout << "description" << description << endl;
+	}
+}
